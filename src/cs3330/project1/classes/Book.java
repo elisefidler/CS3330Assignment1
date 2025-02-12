84608b4d14e6ec75caa290a0912376b82b057275
elisefidler <efidler255@gmail.com>
2025-02-12 17:33:41 -0600

    Created default and parametrized constructors for Book class. Default constructor sets title, author, and ISBN to Unknown and price to 0.0. Parametrized constructor sets variables according to user input.

--- src/cs3330/project1/classes/Book.java
+++ src/cs3330/project1/classes/Book.java
@@ -6,6 +6,20 @@ public class Book {
 	private String ISBN;
 	private double price;
 	
+	public Book() {
+		this.title = "Unknown";
+		this.author = "Unknown";
+		this.ISBN = "Unknown";
+		this.price = 0.0;
+	}
+	
+	public Book(String title, String author, String ISBN, double price) {
+		this.title = title;
+		this.author = author;
+		this.ISBN = ISBN;
+		this.price = price;
+	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
