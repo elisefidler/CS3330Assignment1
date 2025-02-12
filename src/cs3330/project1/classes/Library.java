5dca42259b02c32b655a58500c965f4ef55a2210
elisefidler <efidler255@gmail.com>
2025-02-12 17:48:02 -0600

    Created addBook method. If there are less than 5 books in books array, user input Book object is added and true is returned. If array is full, false is returned.

--- src/cs3330/project1/classes/Library.java
+++ src/cs3330/project1/classes/Library.java
@@ -3,4 +3,13 @@ package cs3330.project1.classes;
 public class Library {
 	private Book[] books = new Book[5];
 	private int count = 0;
+	
+	public boolean addBook(Book book) {
+		if (count < 5) {
+			books[count] = book;
+			count++;
+			return true;
+		}
+		return false;
+	}
 }
