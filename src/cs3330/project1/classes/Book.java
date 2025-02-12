a6b123909db175328c44cf4b0624775278982e59
elisefidler <efidler255@gmail.com>
2025-02-12 17:28:36 -0600

    Created variables for Book classes. Created title, author, ISBN, and price variables for Book class, as well as their respective setter and getter functions.

--- src/cs3330/project1/classes/Book.java
+++ src/cs3330/project1/classes/Book.java
@@ -1,5 +1,40 @@
 package cs3330.project1.classes;
 
 public class Book {
-
+	private String title;
+	private String author;
+	private String ISBN;
+	private double price;
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public void setISBN(String ISBN) {
+		this.ISBN = ISBN;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public String getISBN() {
+		return ISBN;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
 }
