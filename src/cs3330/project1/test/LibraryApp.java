de4dec0f38b8aa2b78398cae10b92a231bf2b0dd
elisefidler <efidler255@gmail.com>
2025-02-12 18:09:41 -0600

    Created new Library and three Books, and added Books to Library using addBook method.

--- src/cs3330/project1/test/LibraryApp.java
+++ src/cs3330/project1/test/LibraryApp.java
@@ -1,9 +1,19 @@
 package cs3330.project1.test;
 
+import cs3330.project1.classes.Book;
+import cs3330.project1.classes.Library;
+
 public class LibraryApp {
 
 	public static void main(String[] args) {
+		Library myLibrary = new Library();
+		Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "‎9780743273565", 6.50);
+		Book book2 = new Book("Pride and Prejudice", "Jane Austen", "‎9780141439518", 5.95);
+		Book book3 = new Book("Moby-Dick", "Herman Melville", "1503280780", 7.00);
 		
+		myLibrary.addBook(book1);
+		myLibrary.addBook(book2);
+		myLibrary.addBook(book3);
 
 	}
 
